e2e774a040e1bee03c029b6c7cc058f3783c18e8
asovchar <a.s.ovchar@gmail.com>
2020-08-25 22:52:40 +0300

    Add simple convertion algorithm

--- converter/__main__.py
+++ converter/__main__.py
@@ -3,8 +3,12 @@ from argparse import ArgumentParser
 from converter import Converter
 from converter.utils.argparse import valid_path
 
+from converter.algorithms import SimpleAlgorithm, ComplexAlgorithm
+
 
 parser = ArgumentParser()
+parser.add_argument("algorithm", type=str, choices=("simple", "complex"),
+                    help="Type of the algorithm to apply")
 parser.add_argument("--input", type=valid_path, required=True,
                     help="Image or directory with images to process")
 parser.add_argument("--output", type=str, required=True,
@@ -13,5 +17,9 @@ parser.add_argument("--output", type=str, required=True,
 
 def main():
     args = parser.parse_args()
-    conv = Converter()
+    if args.algorithm == "complex":
+        algo = ComplexAlgorithm
+    else:
+        algo = SimpleAlgorithm
+    conv = Converter(algorithm=algo)
     conv.run(args.input, args.output)
--- converter/algorithms/simple.py
+++ converter/algorithms/simple.py
@@ -1,5 +1,14 @@
+from numba import njit, uint8
+
+from converter.image import MyImage
 from .base import BaseAlgorithm
 
 
+@njit
+def invert(img: MyImage):
+    return MyImage(uint8(255) - img.array)
+
+
 class SimpleAlgorithm(BaseAlgorithm):
-    pass
+    def process(self, img: MyImage):
+        return invert(img)
